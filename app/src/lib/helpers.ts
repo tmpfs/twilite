9e8acae7c62066c0e9e17f2691d059dd898d4654
muji <muji@tmpfs.org>
2025-09-25 17:32:59 +0800

    Remove unused functions.

--- app/src/lib/helpers.ts
+++ app/src/lib/helpers.ts
@@ -4,11 +4,3 @@ export function formatUtcDateTime(dateTime: string, fmt?: string): string {
   const date = parseISO(dateTime);
   return format(date, fmt ?? "EEE LLL d hh:mm aa");
 }
-
-export function formatUtcMonthYear(dateTime: string): string {
-  return formatUtcDateTime(dateTime, "MM/yyyy");
-}
-
-export function formatUtcDayMonthYear(dateTime: string): string {
-  return formatUtcDateTime(dateTime, "dd/MM/yyyy");
-}
